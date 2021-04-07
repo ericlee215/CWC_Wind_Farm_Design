dc62295ec64c2c0ca3537da1df7c522bd1837c74
Wesley Holt <wesleyjholt@gmail.com>
2021-04-07 17:41:32 -0600

    add functions for generating 2D pareto front using NLopt

---
+++ Optimization/input/pareto/pareto_functions.jl
@@ -0,0 +1,101 @@
+using NLopt, FillArrays
+
+"""
+    NLoptAlgorithmParam(opt_algorithm, opt_tolerance, con_tolerance)
+
+Container to hold algorithm parameters for the NLopt optimization package.
+
+# Arguments
+- `opt_algorithm::Symbol`: Symbol specifying the optimization algorithm, according to the NLopt.jl documentation (https://github.com/JuliaOpt/NLopt.jl#the-opt-type)
+- `opt_tolerance::Float64`: Optimality tolerance
+- `con_tolerance::Float64`: Constraint tolerance
+"""
+struct NLoptAlgorithmParam
+    opt_algorithm
+    opt_tolerance
+    con_tolerance
+    max_eval
+end
+NLoptAlgorithmParam() = NLoptAlgorithmParam(:GN_ISRES, 1e-6, 1e-8, Int(1e6))
+NLoptAlgorithmParam(opt_algorithm) = NLoptAlgorithmParam(opt_algorithm, 1e-6, 1e-8, Int(1e6))
+
+"""
+    ParetoFrontEpsilonConstraintParam(objective_1, objective_2, epsilon)
+
+Container to hold parameters and functions to create a pareto front using the epsilon-constraint method.
+
+# Arguments
+- `objective_1::Function`: First objective function. Takes in a vector of design variables, returns the objective value.
+- `objective_2::Function`: Second objective function. Takes in a vector of design variables, returns the objective value. (This will be used to constrain the first objective.)
+- `epsilon::Float64`
+"""
+struct ParetoFrontEpsilonConstraintParam
+    objective_1
+    objective_2
+    epsilon
+end
+
+"""
+    OptDesignVariablesParam(x, lower_bound, upper_bound)
+
+Container to hold parameters for the design variable for an optimization.
+
+# Arguments
+- `x::Array{Array{Float64,1}}`: Array of design variable vectors. These are the initial starting points for the optimization(s).
+- `lower_bound::Array{Float64,1}`: Vector with the lower bounds for all the design variables.
+- `upper_bound::Array{Float64,1}`: Vector with the upper bounds for all the design variables.
+"""
+struct OptDesignVariablesParam
+    x::Array{Array{Float64,1},1}
+    lower_bound
+    upper_bound
+end
+
+"""
+    generate_2D_pareto_points(design_variables_param::OptDesignVariablesParam, pareto_param::ParetoFrontEpsilonConstraintParam, opt_algorithm_param::NLoptAlgorithmParam)
+
+Generates the 2D Pareto front.
+
+# Arguments
+- `design_variables_param::OptDesignVariablesParam`: Container to hold parameters for the design variable for an optimization.
+- `pareto_param::ParetoFrontEpsilonConstraintParam`: Container to hold parameters and functions to create a pareto front using the epsilon-constraint method.
+- `opt_algorithm_param::NLoptAlgorithmParam`: Container to hold algorithm parameters for the NLopt optimization package.
+"""
+function generate_2D_pareto_points(design_variables_param::OptDesignVariablesParam, pareto_param::ParetoFrontEpsilonConstraintParam, opt_algorithm_param::NLoptAlgorithmParam)
+
+    # get number of epsilon values, starting point samples, and design variables
+    n_epsilon = length(pareto_param.epsilon)
+    n_samples = length(design_variables_param.x)
+    n_designvariables = length(design_variables_param.x[1])
+
+    # define objective and constraint functions for NLopt optimizer
+    obj!(x, gradient) = pareto_param.objective_1(x)
+    con_epsilon!(x, gradient, epsilon) = pareto_param.objective_2(x) - epsilon   # to satisfy the constraint, the value of objective 2 must be below epsilon
+
+    # initialize arrays to hold ouput values
+    fopt = zeros(n_epsilon, n_samples)
+    xopt = zeros(n_epsilon, n_samples, n_designvariables)
+    info = fill(:EmptySymbol, n_epsilon, n_samples)
+
+    # get the pareto front points
+    for i = 1:n_epsilon
+        epsilon = pareto_param.epsilon[i]
+        # create NLopt optimizer object
+        opt = Opt(opt_algorithm_param.opt_algorithm, n_designvariables)
+        # pass objective and constraint functions into optimization object
+        opt.min_objective = obj!
+        inequality_constraint!(opt, (x,g) -> con_epsilon!(x,g,epsilon), opt_algorithm_param.con_tolerance)
+        # set optimization tolerance, bounds for design variables, and maximum allowed function evaluations
+        opt.xtol_rel = opt_algorithm_param.opt_tolerance
+        opt.lower_bounds = design_variables_param.lower_bound
+        opt.upper_bounds = design_variables_param.upper_bound
+        opt.maxeval = opt_algorithm_param.max_eval
+        # run optimizations
+        for j = 1:n_samples
+            x = design_variables_param.x[j]
+            (fopt[i,j], xopt[i,j,:], info[i,j]) = optimize(opt, x)
+        end
+    end
+    
+    return fopt, xopt, info
+end
